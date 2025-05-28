f2ee4bddab9f1eef9a319bde941c310a86737ea4
nqdu <nqdu@foxmail.com>
2025-05-28 18:21:48 -0400

    fix bugs in time window

--- fwatlib/measure/preprocess_sks.py
+++ fwatlib/measure/preprocess_sks.py
@@ -211,7 +211,7 @@ def main():
                 temp = np.zeros((20),'f4')
                 fio.write(
                     f"{sta}.{net} {sta} {net} {ch} i 0 %f %f {temp} %f %f %f 0. 0.\n" %(
-                        win_tb + tref[i] - t_inj,win_te + tref[i] - t_inj, 
+                        -win_tb + tref[i] - t_inj,win_te + tref[i] - t_inj, 
                         misfits[i,-1],misfits[i,0],misfits[i,1]
                     )
                 )
